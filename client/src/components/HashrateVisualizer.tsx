8d8d7c2538bd7b25875d777eb6e8e7220ab6dca5
sashtmitchell <27016459-sashtmitchell@users.noreply.replit.com>
2024-11-21 18:36:33 +0000

    Agent query: Is the title now showing 'The Mind-Blowing Scale of Bitcoin and Elastos' Computing Power' and are all tooltips working when hovering over the scale buttons and network bars?

--- client/src/components/HashrateVisualizer.tsx
+++ client/src/components/HashrateVisualizer.tsx
@@ -172,7 +172,7 @@ const HashrateVisualizer: FC = () => {
         <CardTitle className="flex items-start gap-2 text-lg sm:text-xl">
           <Zap className="w-6 h-6 sm:w-8 sm:h-8 text-yellow-500 shrink-0 mt-1" />
           <span className="leading-tight bg-gradient-to-r from-orange-500 to-yellow-500 bg-clip-text text-transparent">
-            Bitcoin & Elastos Computing Power
+            The Mind-Blowing Scale of Bitcoin and Elastos' Computing Power
           </span>
         </CardTitle>
       </CardHeader>
