15d135be9e6e9e5f531567b4ca22be123028d5b7
sashtmitchell <27016459-sashtmitchell@users.noreply.replit.com>
2024-11-24 06:00:50 +0000

    Agent query: Can you verify: 1. Is the Reserve menu item changed to Buy ELA with a shopping cart icon? 2. Does clicking Buy ELA show the exchanges section?

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'wouter';
-import { Shield, Home, Calculator, TrendingUp, Database } from 'lucide-react';
+import { Shield, Home, Calculator, TrendingUp, ShoppingCart } from 'lucide-react';
 import {
   Sidebar,
   SidebarContent,
@@ -22,7 +22,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     { icon: <Shield className="w-5 h-5" />, label: 'Security', path: '/security' },
     { icon: <Calculator className="w-5 h-5" />, label: 'Value Calc', path: '/value-calc' },
     { icon: <TrendingUp className="w-5 h-5" />, label: 'Supply', path: '/supply' },
-    { icon: <Database className="w-5 h-5" />, label: 'Reserve', path: '/reserve' },
+    { icon: <ShoppingCart className="w-5 h-5" />, label: 'Buy ELA', path: '/buy' },
   ];
 
   return (
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ import HomePage from "./pages/HomePage";
 import SecurityPage from "./pages/SecurityPage";
 import ValueCalcPage from "./pages/ValueCalcPage";
 import SupplyPage from "./pages/SupplyPage";
-import ReservePage from "./pages/ReservePage";
+import BuyPage from "./pages/BuyPage";
 
 function Router() {
   return (
@@ -20,7 +20,7 @@ function Router() {
         <Route path="/security" component={SecurityPage} />
         <Route path="/value-calc" component={ValueCalcPage} />
         <Route path="/supply" component={SupplyPage} />
-        <Route path="/reserve" component={ReservePage} />
+        <Route path="/buy" component={BuyPage} />
         <Route>404 Page Not Found</Route>
       </Switch>
     </Layout>
